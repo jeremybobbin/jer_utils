f7ac8a8440652b9dd81ed385e2b5ccab26eeaf63
Jeremy Bobbin <jeremybobbin@gmail.com>
2019-08-23 08:46:38 -0700

    add dimensional array function

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+fn _dimensional_array<'a>(output: &'a mut String, n: u64, _type: &'a str, width: u64) {
+    if n > 0 {
+        output.push_str("[");
+        _dimensional_array(output, n - 1, _type, width);
+        output.push_str(&format!("{}]; ", width));
+    } else {
+        output.push_str(_type);
+        output.push_str("; ");
+    }
+}
+
+// Function to help visualize multidimensional arrays.
+// I may replace 'width' with a closure that returns a u64...
+pub fn dimensional_array<'a>(n: u64, _type: &'a str, width: u64) -> String {
+    let mut output = String::new();
+    _dimensional_array(&mut output, n, _type, width);
+    output
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn dimensional_array_works() {
+        super::dimensional_array(10, "Option<u32>", 20);
+        let blah: Option<u32> = None;
+        assert_eq!(2 + 2, 4);
+    }
+}
